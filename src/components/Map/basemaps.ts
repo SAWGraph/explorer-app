c6dd9aba8d06df119598f3d3435c757a073306b3
Prayas Lashkari <prayas.lashkari98@gmail.com>
2026-04-24 00:35:47 -0400

    feat: replace Terrain + Hydro with combined Hydro basemap
    Merges the separate Terrain and Terrain + Hydro basemap options into a
    single Hydro option that stacks the hydro tile layer on top of the shaded
    relief terrain base. Also removes the standalone Terrain entry.

--- src/components/Map/basemaps.ts
+++ src/components/Map/basemaps.ts
@@ -3,6 +3,8 @@ export interface BasemapOption {
   label: string;
   url: string;
   attribution: string;
+  maxNativeZoom?: number;
+  overlayUrl?: string;
 }
 
 export const BASEMAPS: BasemapOption[] = [
@@ -33,6 +35,24 @@ export const BASEMAPS: BasemapOption[] = [
     url: 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
     attribution:
       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/">CARTO</a>',
+  },
+  {
+    key: 'imagery',
+    label: 'Imagery',
+    url: 'https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryTopo/MapServer/tile/{z}/{y}/{x}',
+    attribution:
+      'Tiles courtesy of the <a href="https://usgs.gov/">U.S. Geological Survey</a>',
+    maxNativeZoom: 16,
+  },
+  {
+    key: 'hydro',
+    label: 'Hydro',
+    url: 'https://basemap.nationalmap.gov/arcgis/rest/services/USGSShadedReliefOnly/MapServer/tile/{z}/{y}/{x}',
+    attribution:
+      'Tiles courtesy of the <a href="https://usgs.gov/">U.S. Geological Survey</a>',
+    maxNativeZoom: 16,
+    overlayUrl:
+      'https://basemap.nationalmap.gov/arcgis/rest/services/USGSHydroCached/MapServer/tile/{z}/{y}/{x}',
   },
 ];
 
